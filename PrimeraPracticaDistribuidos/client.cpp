419c3f728feaaf0071365681e32ec057c3ecd85c
D1n0sXIX <alej.mlm03@gmail.com>
2026-06-08 14:55:15 +0200

    Server para la primera parte funcional

--- PrimeraPracticaDistribuidos/client.cpp
+++ PrimeraPracticaDistribuidos/client.cpp
@@ -3,8 +3,135 @@
 #include <string>
 #include <thread>
 #include <list>
+#include <sstream>
+#include <fstream>
+#include <experimental/filesystem>
+
+
+
+#define SERVERPORT 1234
+#define LOCALHOST "127.0.0.1"
+using namespace std;
+using namespace std::experimental::filesystem;
+
+enum Command {
+    list_files, // lls --> listFile()
+    read_file,  // download --> readFile()
+    write_file  // upload --> writeFile()
+};
 
 int main(int argc, char** argv)
 {
- return 0;
+// 1. Conectamos al servidor
+connection_t conn = initClient(LOCALHOST, SERVERPORT);
+
+if(conn.socket==-1){
+    cout<<"Error connecting to server"<<endl;
+    return -1;
+}
+cout<<"Connected to server with id "<<conn.serverId<<endl;
+
+//Creamos el vector donde se almacenaran los comandos
+string command;
+string fileName;
+string line;
+
+do{
+    cout<< ": ";
+    getline(cin, line);     // 1. Leer la linea entera
+    stringstream ss(line);   // 2. meterla en el "tubo"
+    ss >> command;           // 3. Sacamos el comando
+
+    if (command == "lls"){
+        vector<unsigned char> buffer;
+        pack(buffer, list_files);
+        sendMSG<unsigned char>(conn.serverId, buffer);
+
+        // Mostramos la respuesta
+        vector<unsigned char> response;
+        recvMSG<unsigned char>(conn.serverId, response);
+
+        // Desempaquetamos
+        size_t numFiles = unpack<size_t>(response);             // en forma de bucle
+        for (size_t i = 0; i < numFiles; i++){
+            string name;
+            name.resize(unpack<size_t>(response));              // longitud de este nombre
+            unpackv(response, (char*)name.data(), name.size()); // su nombre
+            cout << name << endl;
+        }
+
+    }
+    else if(command == "download"){
+        ss >> fileName; // Sacamos el nombre del archivo a descargar
+
+        vector<unsigned char> buffer;
+        pack(buffer, read_file); // Empaquetamos el comando
+        pack(buffer, fileName.size()); // Empaquetamos la longitud del nombre del archivo
+        packv(buffer, fileName.data(), fileName.size()); // Empaquetamos el nombre del archivo
+        sendMSG<unsigned char>(conn.serverId, buffer); // Enviamos el comando al servidor
+
+        // Recibimos los bytes del fichero
+        vector<unsigned char> response;
+        recvMSG<unsigned char>(conn.serverId, response);
+
+        vector<unsigned char> fileData;
+        fileData.resize(unpack<size_t>(response)); // Desempaquetamos la longitud del fichero
+        unpackv(response, fileData.data(), fileData.size()); // Desempaquetamos
+        
+
+        ofstream file(fileName, ios::out | ios::binary);
+        if(file.is_open()){
+            file.write((char*)fileData.data(), fileData.size());
+            file.close();
+            cout << "Descargado: " << fileName << " (" << fileData.size() << " bytes)" << endl;
+        }else{
+            cout << "ERROR: no se pudo crear el fichero " << fileName << endl;
+        }
+    }
+    else if(command == "upload"){
+        ss >> fileName; // Sacamos el nombre del archivo a subir
+
+        ifstream file(fileName, ios::in | ios::binary);
+        if(file.is_open()){
+            vector<unsigned char> fileData((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+            file.close();
+
+            vector<unsigned char> buffer;
+            pack(buffer, write_file); // Empaquetamos el comando
+            pack(buffer, fileName.size()); // Empaquetamos la longitud del nombre del archivo
+            packv(buffer, fileName.data(), fileName.size()); // Empaquetamos el nombre del archivo
+            pack(buffer, fileData.size()); // Empaquetamos la longitud del fichero
+            packv(buffer, fileData.data(), fileData.size()); // Empaquetamos el contenido del fichero
+            sendMSG<unsigned char>(conn.serverId, buffer); // Enviamos el comando al servidor
+
+            // El server nos confirma
+            vector<unsigned char> response;
+            recvMSG<unsigned char>(conn.serverId, response);
+            bool ok = unpack<bool>(response);
+            if(ok){
+                cout << "Subido: " << fileName << " (" << fileData.size() << " bytes)" << endl;
+            }else{
+                cout << "ERROR: no se pudo subir el fichero " << fileName << endl;
+            }
+        }
+    }
+    else if(command == "ls"){
+        cout<<"Listing files in local path"<<endl;
+			experimental::filesystem::path directorypath="./";
+
+			for (const auto& entry : 
+					 directory_iterator(directorypath)) { 
+					 if(is_regular_file(entry))
+						cout<<entry.path()<<endl; 
+				} 
+			cout<<endl;
+    }
+    else if(command == "exit()"){
+        cout << "Exiting..." << endl;
+        closeConnection(conn.serverId);
+    }
+
+
+}while(command != "exit()");
+return 0;
 }
--- PrimeraPracticaDistribuidos/server.cpp
+++ PrimeraPracticaDistribuidos/server.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 #define SERVERPORT 1234
+
 FileManager fm("FileManagerDir");
 mutex fm_mutex;
 
@@ -45,14 +46,26 @@ while(!salir){
 }
 
 void atenderCliente(int clientID){
-    // 1. Recibir el comando del cliente
-    vector<unsigned char> buffer;
+    while(true){
+        // 1. Recibir el comando del cliente
+        vector<unsigned char> buffer;
         // recvMSG, unpack del Command
         recvMSG<unsigned char>(clientID, buffer);
+
+        
+        if(buffer.empty()){
+            cout<<"Client "<<clientID<<" disconnected"<<endl;
+            return;
+        }
+
+        
+
         Command cmd = unpack<Command>(buffer);
         cout << "Cliente " << clientID << " pidio comando: " << cmd << endl;
-    // 2. Ejecutar el comando y enviar la respuesta al cliente
+
+        // 2. Ejecutar el comando y enviar la respuesta al cliente
         switch(cmd){
+
             case list_files:{
                 fm_mutex.lock();
                 vector<string> files = fm.listFiles();
@@ -60,13 +73,13 @@ void atenderCliente(int clientID){
                 // Empaquetar la respuesta
                 vector<unsigned char> response;
                 pack(response, files.size());
-                for (const string& f : files) {
-                    pack(response, f.size());
-                    packv(response, f.data(), f.size());
-                }
+                    for (const string& f : files) {
+                        pack(response, f.size());
+                        packv(response, f.data(), f.size());
+                    }
                 sendMSG<unsigned char>(clientID, response);
                 break;
-            }
+                }
 
             case read_file:{
                 // Obtenemos el nombre del archivo a leer
@@ -85,15 +98,39 @@ void atenderCliente(int clientID){
                 pack(response, fileData.size());
                 packv(response, fileData.data(), fileData.size());
                 sendMSG<unsigned char>(clientID, response);
-            break;
-            }
-            
-            case write_file:
-            break;
+                break;
+                }
 
-            default:
-                cout<<"Unknown command from client "<<clientID<<endl;
-            break;
+            case write_file:{
+                // Sacamos el nombre del archivo a escribir
+                string fileName;
+                fileName.resize(unpack<size_t>(buffer));
+                unpackv(buffer, (char*)fileName.data(), fileName.size());
+                    
+                // Sacamos el contenido del archivo a escribir
+                vector<unsigned char> fileData;
+                fileData.resize(unpack<size_t>(buffer));
+                unpackv(buffer, fileData.data(), fileData.size());
+                    
+                // Escribimos el archivo
+                fm_mutex.lock();
+                fm.writeFile(fileName, fileData);
+                fm_mutex.unlock();
+
+                // Enviamos una respuesta de confirmacion (puede ser un mensaje vacio)
+                cout << "Cliente " << clientID << " escribio el archivo: " << fileName << endl;
+                vector<unsigned char> response;
+                bool ok = true;
+                pack(response, ok);
+                sendMSG<unsigned char>(clientID, response);
+            
+                break;
+                }
+
+                default:{
+                    cout<<"Unknown command from client "<<clientID<<endl;
+                break;
+                }
         }
-    // 3. Empaquetar la respuesta y enviarla al cliente
+    }
 }
